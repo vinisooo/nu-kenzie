b97509e0c6f0d290425551cb5efc910d4fbf78c9
vinisooo <viniciuslira9397@gmail.com>
2022-11-18 10:54:53 -0400

    fix

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -2,13 +2,18 @@ import './style.css'
 
 const FilterTransactions = ({filter, setFilter})=>{
 
+    function filterThis(event){
+        setFilter(event.target.value);
+        event.target.classList.remove("grey-btn")
+    }
+
     return (
         <div className="filter-div">
-            <h2>Resumo Finanaceiro</h2>
+            <h2>Resumo Financeiro</h2>
             <nav className="filters">
-                <button className="default-btn" value="all" onClick={(e)=>setFilter(e.target.value)}>Todos</button>
-                <button className="default-btn grey-btn" value="entrada" onClick={(e)=>setFilter(e.target.value)}>Entradas</button>
-                <button className="default-btn grey-btn" value="saida" onClick={(e)=>setFilter(e.target.value)}>Despesas</button>
+                <button className="default-btn" value="all" onClick={filterThis}>Todos</button>
+                <button className="default-btn grey-btn" value="entrada" onClick={filterThis}>Entradas</button>
+                <button className="default-btn grey-btn" value="saida" onClick={filterThis}>Despesas</button>
             </nav>
         </div>
         
